9604a6f2d91593becbde050b27f9720e83133833
Rafael Pacífico <rafael@webstylus.com.br>
2022-06-28 14:11:14 -0300

    feat: create siderbar and content component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from "./Button";
+import {useEffect, useState} from "react";
+import {api} from "../services/api";
+
+export interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface Props {
+    onPress: (id: number) => void,
+    selectedGenreId: number
+}
+
+export function SideBar({onPress, selectedGenreId}: Props) {
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+            setGenres(response.data);
+        });
+    }, []);
+
+    return (
+        <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+
+            <div className="buttons-container">
+                {genres.map(genre => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => onPress(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    );
 }
